973d6228d92c459cb07f2550dafd5837d6b9313c
jrevete-mslabs <jrevete@marketstreetlabs.com>
2025-05-09 23:12:50 -0400

    fix: modificar modulos

--- manipularmodulos.sh
+++ manipularmodulos.sh
@@ -7,47 +7,44 @@ NC='\e[0m'
 function activarmod
 {
 	#Verificar si el modulo esta desactivado o no
-	result=$(cat /proc/modules | grep $1)
-        modulo=$(echo $result | awk '{print $1}')
-        if [ "$modulo" == "$1" ] ; then
+	result=$(lsmod | awk '{print $1}' | grep -w $1)
+    if [ "$modulo" == "$1" ] ; then
 		echo "El modulo $1 ya esta activado"
 	else
 		sudo modprobe $1
-                echo "El modulo $1 ha sido activado"
+        echo "El modulo $1 ha sido activado"
 	fi
 }
 
-#Funcion para activar el modulo dado
+#Funcion para desactivar el modulo dado
 function desactivarmod
 {
 	#Verificar si el modulo esta desactivado o no
-	result=$(cat /proc/modules | grep $1)
-        modulo=$(echo $result | awk '{print $1}')
-        if [ "$modulo" == "$1" ] ; then
-		sudo modprobe -r $1
-                echo "El modulo $1 ha sido desactivado"
-        else
-                echo "El modulo $1 ya esta desactivado"
-        fi
+	result=$(lsmod | awk '{print $1}' | grep -w $1)
+    if [ "$result" == "$1" ] ; then
+	sudo modprobe -r $1
+        echo "El modulo $1 ha sido desactivado"
+    else
+        echo "El modulo $1 ya esta desactivado"
+    fi
 }
 
 #Funcion para explicar el script y dar ayuda
 function ayuda
 {
 	echo -e "${blue}MANIPULAR MODULOS\n\nEste script tiene la funcion de ayudarte a activar y desactivar los modulos de tu ordenador, empleandolo como un comando (ejecutandolo con flags o el nombre del modulo).${NC}"
-	echo -e "flags:\n-a    Se encarga de verificar si el modulo esta desactivado y, en ese caso, activarlo\n\nSintasis: ./manipularmodulos.sh -a [nombre modulo]\n\n-r    Se encarga de verificar si el modulo esta activado y, en ese caso, desactivarlo\n\nSintasis: ./manipularmodulos.sh -r [nombre modulo]\n\n-help    Se encarga de proporcionar informacion de como usar el script\n\nSintasis: ./manipularmodulos.sh -help\n\n[nombre modulo]    Se encarga proporcionar informacion acerca del modulo, sabiendo si esta cativado o desactivado\n\nSintasis: ./manipularmodulos.sh [nombre modulo]"
+	echo -e "	flags:\n		-a    Se encarga de verificar si el modulo esta desactivado y, en ese caso, activarlo\n		Sintasis: ./manipularmodulos.sh -a [nombre modulo]\n\n\n		-r    Se encarga de verificar si el modulo esta activado y, en ese caso, desactivarlo\n		Sintasis: ./manipularmodulos.sh -r [nombre modulo]\n\n\n		-help    Se encarga de proporcionar informacion de como usar el script\n		Sintasis: ./manipularmodulos.sh -help\n\n\n		[nombre modulo]    Se encarga proporcionar informacion acerca del modulo, sabiendo si esta activado o desactivado\n		Sintasis: ./manipularmodulos.sh [nombre modulo]"
 }
 
 #Funcion para saber el estado de un modulo
 function infomodl
 {
-	result=$(cat /proc/modules | grep $1)
-        modulo=$(echo $result | awk '{print $1}')
-        if [ "$modulo" == "$1" ] ; then
-	        echo "El modulo $1 esta activado"
-        else
-                echo "El modulo $1 esta desactivado"
-        fi
+	result=$(lsmod | awk '{print $1}' | grep -w $1)
+    if [ "$result" == "$1" ] ; then
+	    echo "El modulo $1 esta activado"
+    else
+        echo "El modulo $1 esta desactivado"
+    fi
 
 }
 #Verificar si los parametros de entrada necesarios para cada caso estan dados
