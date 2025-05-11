3db949952b7b09136912afc549204802d64a6ccd
jrevete-mslabs <jrevete@marketstreetlabs.com>
2025-05-11 05:25:55 -0400

    fix: registrar usb (falta)

--- registrarusb.sh
+++ registrarusb.sh
@@ -1,146 +1,73 @@
-
 #!/bin/bash
 
-#Ruta del archivo donde se guardaran los logs
-SCRIPT_PATH="$0"
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-LOG_FILE="$SCRIPT_DIR/registro.log"
-#Ruta del archivo regla udev
-reglaudev="/etc/udev/rules.d/99-usb-custom.rules"
+# Configuración
+archivoRegistros="/var/log/logsUSB.log"
+direccionUDEV="/etc/udev/rules.d/99-usb-custom.rules"
+ubicacionScript="/usr/local/bin/scriptUSB.sh"
 
-#Verificando si las reglas udev ya existen, si no se crean
-if [ ! -e "$reglaudev" ] ; then
-	sudo touch "$reglaudev"
-	sudo chmod 666 "$reglaudev"
-	echo "#Al conectar un usb en los puertos usb, se ejecuta el script" >> "$reglaudev"
+# Verificar si el script ya existe, si no, crearlo
+if [[ ! -f $ubicacionScript ]] ; then
+    sudo tee "$ubicacionScript" > /dev/null <<'EOF'
+#!/bin/bash
 
-	actionadd='ACTION=="add",'
-	sub='SUBSYSTEMS=="usb",'
-	envi='ENV{DEVTYPE}=="usb_device",'
-	bin='"/bin/bash'
-	ruta='/registrarusb.sh"'
+archivoRegistros="/var/log/logsUSB.log"
+puertoID=$(basename "$(dirname "$DEVNAME")")
+fecha=$(date "+%Y/%m/%d %a %I:%M %p")
 
-	echo "$actionadd $sub $envi RUN+=$bin $HOME$ruta" >> "$reglaudev"
-
-	actionrem='ACTION=="remove",'
-
-	echo "" >> "$reglaudev"
-	echo "#Al retirar un usb en los puertos usb, se ejecuta el script" >> "$reglaudev"
-	echo "$actionrem $sub $envi RUN+=$bin $HOME$ruta" >> "$reglaudev"
-	sudo chmod 644 "$reglaudev"
-	sudo service udev restart
+# Verificar si el puertoID es válido
+if [ -z "$puertoID" ] ; then
+	exit 0
+fi
+if [ "$puertoID" = "." ] ; then
+    exit 0
 fi
 
-#Verificando si el archivo con los logs ya tiene el encabezado
-if [ ! -e "$LOG_FILE" ] ; then
-	echo " PUERTO   |    FECHA/HORA CONEXION     |  FECHA/HORA DESCONEXION" > "$LOG_FILE"
-else
-	echo ""
-fi
-
-#Puerto ingresado por un usb
-devpath1="$DEVPATH"
-devpath=$(basename "$devpath1")
-porto=$(echo "$devpath" | cut -d "-" -f2)
-
-port1=$(lsusb -tv | grep "Port 1" | grep -i "usb" | awk '{print $2}')
-
-port3=$(lsusb -tv | grep "Port 3" | grep -i "usb" | awk '{print $2}')
-
-port4=$(lsusb -tv | grep "Port 4" | grep -i "usb" | awk '{print $2}')
-
-#Modificando la fecha a como lo pide la tarea
-fecha=$(date)
-ano=$(echo "$fecha" | awk '{print $6}')
-dia=$(echo "$fecha" | awk '{print $3}')
-diasem=$(echo "$fecha" | awk '{print $1}')
-mes=$(echo "$fecha" | awk '{print $2}')
-case "$mes" in
-	"Ene")mes="01";;
-	"Feb")mes="02";;
-	"Mar")mes="03";;
-	"Abr")mes="04";;
-	"May")mes="05";;
-	"Jun")mes="06";;
-	"Jul")mes="07";;
-	"Ago")mes="08";;
-	"Sep")mes="09";;
-	"Oct")mes="10";;
-	"Nov")mes="11";;
-	"Dic")mes="12";;
+case "$1" in
+    "1")
+        # USB conectado
+        echo "PUERTO$puertoID | $fecha | " | column -t -s " " >> "$archivoRegistros" ;;
+    "0")
+        # Verificacion de desconexión
+		# Verificar si el puertoID ya tiene una entrada en el log
+        ultimoRegistro=$(cat "$archivoRegistros" | grep "PUERTO$puertoID |.*| $" | tail -n 1)
+		numeracionLineaUltimoRegistro=$(cat -n "$archivoRegistros" | grep "PUERTO$puertoID |.*| $" | tail -n 1 )
+		numeroLineaUltimoRegistro=$(echo "$numeracionLineaUltimoRegistro" | awk '{print $1}')
+		
+        if [ -n "$ultimoRegistro" ] ; then
+            # Completar la entrada existente
+            archivoTemporal=$(mktemp)
+			# Extraer la fecha de conexión y puertoID
+			puerto=$(echo "$ultimoRegistro" | awk -F'|' '{print $1}' | xargs)
+			fechaConexion=$(echo "$ultimoRegistro" | awk -F'|' '{print $2}' | xargs)
+			# Editar el registro existente con la fecha de desconexión
+			sed -i '$numeroLineaUltimoRegistros/.*/PUERTO$puertoID | $fechaConexion | $fecha/' "$archivoRegistros"
+			cat "$archivoRegistros" > "$archivoTemporal"
+			sudo mv "$archivoTemporal" "$archivoRegistros"
+        else
+            # Registrar desconexión sin conexión previa
+            echo "PUERTO$puertoID | | $fecha" | column -t -s " " >> "$archivoRegistros"
+        fi
+        ;;
 esac
+EOF
 
-hora=$(echo "$fecha" | awk '{print $4}')
-primhor=$(echo "$hora" | cut -d ":" -f1)
-seghor=$(echo "$hora" | cut -d ":" -f2)
-if [ "$primhor" -ge 12 ] ; then
-	phor="PM"
-	if [ "$primhor" -gt 12 ] ; then
-		hor=$(($primhor - 12))
-
-	else
-		hor="12"
-	fi
-else
-	phor="AM"
-	hor=$primhor
+    sudo chmod +x "$ubicacionScript"
 fi
 
-tiempo="$ano/$mes/$dia $diasem $hor:$seghor $phor"
-#Casos de conexion en puertos
-case "$porto" in
-	"1")
-	#Comprobando en el archivo de los logs si el usb se esta ingresando o no
-	if [ "$port1" == "Port" ] ; then
-		echo " PUERTO1  |  $tiempo  |  ------" >> "$LOG_FILE"
-	else
-		complastlog=$(cat "$LOG_FILE" | grep "PUERTO1" | tail -1 | awk '{print $8}')
-		complastnum=$(cat -n "$LOG_FILE" | grep "PUERTO1" | tail -1 | awk '{print $1}')
-		#Como el puerto no se estaba ingresando, se comprueba si al retirar en ese puerto ya habia un log que no se cerro o el log se hizo cuando la pc estaba apagada
-		if [ "$complastlog" == "------" ] ; then
-			timebef=$(cat "$LOG_FILE" | grep "PUERTO1" | tail -1 | cut -d "|" -f2)
-			echo " PUERTO1  |$timebef|  $tiempo  " >> "$LOG_FILE"
-			sed -i "$complastnum d" "$LOG_FILE"
-		else
-			#Log cuando la pc estaba apagada
-			echo " PUERTO1  |           ------           |  $tiempo" >> "$LOG_FILE"
-		fi
-	fi;;
-	"3")
-	#Comprobando en el archivo de los logs si el usb se esta ingresando o no
-	if [ "$port3" == "Port" ] ; then
-                echo " PUERTO3  |  $tiempo  |  ------" >> "$LOG_FILE"
-        else
-                complastlog=$(cat "$LOG_FILE" | grep "PUERTO3" | tail -1 | awk '{print $8}')
-                complastnum=$(cat -n "$LOG_FILE" | grep "PUERTO3" | tail -1 | awk '{print $1}')
-		#Como el puerto no se estaba ingresando, se comprueba si al retirar en ese puerto ya habia un log que no se cerro o el log se hizo cuando la pc estaba apagada
-                if [ "$complastlog" == "------" ] ; then
-			timebef=$(cat "$LOG_FILE" | grep "PUERTO3" | tail -1 | cut -d "|" -f2)
-                        echo " PUERTO3  |$timebef|  $tiempo  " >> "$LOG_FILE"
-                        sed -i "$complastnum d" "$LOG_FILE"
+# Crear archivo de reglas udev si no existe
+if [ ! -f "$UDEV_RULES_PATH" ] ; then
+    sudo tee "$direccionUDEV" > /dev/null <<'EOF'
+ACTION=="add", SUBSYSTEM=="usb", RUN+="/usr/local/bin/scriptUSB.sh 1"
+ACTION=="remove", SUBSYSTEM=="usb", RUN+="/usr/local/bin/scriptUSB.sh 0"
+EOF
+    sudo udevadm control --reload-rules
+    sudo udevadm trigger
+fi
 
-               	else
-			#Log cuando la pc estaba apagada
-                       	echo " PUERTO3  |           ------           |  $tiempo" >> "$LOG_FILE"
+if [[ ! -f $archivoRegistros ]] ; then
+    # Crear archivo de log y dar permisos
+    sudo touch "$archivoRegistros"
+    sudo chmod 666 "$archivoRegistros"
+    echo "PUERTOS | FECHA_CONEXION | FECHA_DESCONEXION" | column -t >> $archivoRegistros
 
-		fi
-	fi;;
-
-	"4")
-	#Comprobando en el archivo de los logs si el usb se esta ingresando o no
-        if [ "$port4" == "Port" ] ; then
-                echo " PUERTO4  |  $tiempo  |  ------" >> "$LOG_FILE"
-        else
-                complastlog=$(cat "$LOG_FILE" | grep "PUERTO4" | tail -1 | awk '{print $8}')
-                complastnum=$(cat -n "$LOG_FILE" | grep "PUERTO4" | tail -1 | awk '{print $1}')
-		#Como el puerto no se estaba ingresando, se comprueba si al retirar en ese puerto ya habia un log que no se cerro o el log se hizo cuando la pc estaba apagada
-                if [ "$complastlog" == "------" ] ; then
-                        timebef=$(cat "$LOG_FILE" | grep "PUERTO4" | tail -1 | cut -d "|" -f2)
-                        echo " PUERTO4  |$timebef|  $tiempo  " >> "$LOG_FILE"
-                        sed -i "$complastnum d" "$LOG_FILE"
-                else
-			#Log cuando la pc estaba apagada
-                        echo " PUERTO4  |           ------           |  $tiempo" >> "$LOG_FILE"
-		fi
-	fi;;
-	esac
+fi
